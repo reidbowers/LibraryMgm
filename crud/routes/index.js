01c9455f21c0066cead92015389b8e9f81c21ef8
reidbowers <reid.bowers1103@gmail.com>
2023-03-04 23:31:20 -0500

    search implement pt1
    Basic features of autocomplete search implemented

--- crud/routes/index.js
+++ crud/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var database = require('../database');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+
+});
+
+router.get('/get_data', function(request,response, next){
+
+    var search_query = request.query.search_query;
+
+    var query = `
+    SELECT title FROM media
+    WHERE title LIKE '%${search_query}%'
+    LIMIT 10
+    `;
+
+    database.query(query, function(error,data){
+
+        response.json(data);
+        
+    })
 });
 
 module.exports = router;
